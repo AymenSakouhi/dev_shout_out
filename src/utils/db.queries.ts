8a6ce7f38d7815f969acb8faa15e858d9cd7c6c5
Aymen Sakouhi <aymenmarketer@gmail.com>
2024-04-28 16:46:32 +0200

    adding no implicit any rule

--- src/utils/db.queries.ts
+++ src/utils/db.queries.ts
@@ -3,6 +3,7 @@ import { supabase } from '@/utils/db.config'
 
 export const getAllData = async (
   table: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   setStateFunc: React.Dispatch<React.SetStateAction<any[] | null>>
 ) => {
   const { data }: PostgrestSingleResponse<unknown[]> = await supabase
